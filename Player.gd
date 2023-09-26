05c71faf4ce4e88eb0a9fc2d445b806f132b1651
cfrak <cfrak@DESKTOP-FEVU2I5>
2023-09-26 16:43:02 -0500

    add the stuff

---
+++ Player.gd
@@ -0,0 +1,18 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
+
+	if Input.is_action_just_pressed("jump"):
+		apply_impulse(Vector3.UP * 10.0, Vector3.ZERO)
