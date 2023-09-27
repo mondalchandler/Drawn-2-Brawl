e652156438117add0cc119e9f13a65b83e00c913
cfrak <cfrak@DESKTOP-FEVU2I5>
2023-09-27 09:18:16 -0500

    added logic for going off the map, also character moves relative to camera pos

--- Player.gd
+++ Player.gd
@@ -1,18 +1,42 @@
 extends RigidBody3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+@onready var cam = get_viewport().get_camera_3d()
 
+func get_camera_relative_input(input) -> Vector3:
+	var cam_right = cam.global_transform.basis.x
+	var cam_forward = cam.global_transform.basis.z
+	# make cam_forward horizontal:
+	cam_forward = cam_forward.slide(Vector3.UP).normalized()
+	# return camera relative input vector:
+	return cam_forward * input.z + cam_right * input.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input := Vector3.ZERO
+	var relative_input
+	
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-	
-	apply_central_force(input * 1200.0 * delta)
+	relative_input = get_camera_relative_input(input)
 
 	if Input.is_action_just_pressed("jump"):
-		apply_impulse(Vector3.UP * 10.0, Vector3.ZERO)
+		apply_impulse(Vector3.UP * 20.0, Vector3.ZERO)
+	
+	apply_central_force(relative_input * 2000.0 * delta)
+	apply_central_force(Vector3.DOWN * 9.8 * mass)
+
+	# Create a "Pac-Man" effect where if the player goes off one side of the stage, they appear on the other side.
+	if position.x < -5.0:
+		position.x = 5.0
+	elif position.x > 5.0:
+		position.x = -5.0
+	elif position.z < -5.0:
+		position.z = 5.0
+	elif position.z > 5.0:
+		position.z = -5.0
+	else:
+		pass
