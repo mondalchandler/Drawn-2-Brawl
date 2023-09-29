659396df941a932074ff82f5003b80a8d82068aa
cfrak <cfrak@DESKTOP-FEVU2I5>
2023-09-29 12:07:51 -0500

    added test attack (doesn't do damage rn) + dummy area

--- Player.gd
+++ Player.gd
@@ -1,10 +1,12 @@
 extends RigidBody3D
 
+@onready var cam = get_viewport().get_camera_3d()
+@onready var anim_player = $AnimationPlayer
+@onready var hitbox = $MeshInstance3D2/Hitbox
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
-@onready var cam = get_viewport().get_camera_3d()
 
 func get_camera_relative_input(input) -> Vector3:
 	var cam_right = cam.global_transform.basis.x
@@ -13,6 +15,16 @@ func get_camera_relative_input(input) -> Vector3:
 	cam_forward = cam_forward.slide(Vector3.UP).normalized()
 	# return camera relative input vector:
 	return cam_forward * input.z + cam_right * input.x
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "melee_attack":
+		print("in here")
+		anim_player.play("idle")
+		hitbox.monitoring = false
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("enemy"):
+		print("enemy hit")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,6 +37,11 @@ func _process(delta):
 
 	if Input.is_action_just_pressed("jump"):
 		apply_impulse(Vector3.UP * 20.0, Vector3.ZERO)
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("melee_attack"):
+		anim_player.play("melee_attack")
+		hitbox.monitoring = true
 	
 	apply_central_force(relative_input * 2000.0 * delta)
 	apply_central_force(Vector3.DOWN * 9.8 * mass)
