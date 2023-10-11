b2bdf5ff6bb363aed12bdacd43c24c35a687ba3f
mondalchandler <cfrakes2000@gmail.com>
2023-10-10 19:35:26 -0500

    quick menu fix from renaming files

--- src/Scenes/UI/main_menu.gd
+++ src/Scenes/UI/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://src/Scenes/Levels/TestLevel.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://src/Scenes/UI/OptionsMenu.tscn")
+	get_tree().change_scene_to_file("res://src/Scenes/UI/options_menu.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
--- src/Scenes/UI/options_menu.gd
+++ src/Scenes/UI/options_menu.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://src/Scenes/UI/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://src/Scenes/UI/main_menu.tscn")
