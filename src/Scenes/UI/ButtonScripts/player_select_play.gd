d52bc51fcd68e792908049d591bb1a822db252b7
Kyle Senebouttarath <kylesenebouttarath@gmail.com>
2023-11-27 17:22:33 -0600

    preMeeting changes

--- src/Scenes/UI/ButtonScripts/player_select_play.gd
+++ src/Scenes/UI/ButtonScripts/player_select_play.gd
@@ -16,6 +16,8 @@ func run_task():
 			saloon_scene.players.append(test_dummy)
 			var test_dummy2 = load("res://src/Scenes/Objects/DummyEnemy.tscn")
 			saloon_scene.players.append(test_dummy2)
+			var test_dummy3 = load("res://src/Scenes/Objects/DummyEnemy.tscn")
+			saloon_scene.players.append(test_dummy3)
 		
 		var saloon = main_scene._change_scene("res://src/Scenes/Levels/SaloonMap.tscn", saloon_setup)
 		saloon.spawn_players()
