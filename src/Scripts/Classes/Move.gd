aba6eb3a828930ebb7c7011e243db8b623d366ec
mondalchandler <cfrakes2000@gmail.com>
2023-11-30 21:28:42 -0600

    re-added documentation to code to make it more clear of the standards

--- src/Scripts/Classes/Move.gd
+++ src/Scripts/Classes/Move.gd
@@ -1,5 +1,20 @@
 # Chandler Frakes
 
+# Moves are the basic building blocks of combat in the game. They will be instantiated
+# whenever a player presses an attack button, and are then passed to the combat system
+# which handles the rest of the logic.
+
+# A move is defined using the following format:
+# [
+#	MOVE NAME: String,
+#	MOVE TYPE: String,
+#	MOVE DATA: Array (see below for format of each type)
+# ]
+
+# HOW MOVE DATA IS STRUCTURED FOR EACH TYPE
+# MELEE (i.e. Regular Hitbox):		[offset, dmg, hitstun, kb_length, kb_stg, hitbox_size]
+# HITSCAN (TODO):					[dmg, hitstun, kb_stg]
+
 class_name Move
 extends Node
 
