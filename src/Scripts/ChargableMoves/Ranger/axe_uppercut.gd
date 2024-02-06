b947315508a5f570e5ecb805efc5f8e7a03b99d4
otteliena <otteliena@msoe.edu>
2024-02-06 11:00:14 -0600

    Charge code

---
+++ src/Scripts/ChargableMoves/Ranger/axe_uppercut.gd
@@ -0,0 +1,30 @@
+extends Move
+
+var damage_range_holder = [0, 0]
+var y_knockback_holder = 0
+@export var max_damage: int = 30
+@export var max_y_knockback: int = 60
+@export var charge_time: int = 5
+var time_passed = 0
+
+
+func move_charge_effect(delta):
+	time_passed+=delta
+	if time_passed < charge_time:
+		self.hitbox.damage_range[1] = (time_passed/charge_time)*(max_damage-damage_range_holder[1]) + damage_range_holder[1]
+		self.hitbox.damage_range[0] = (time_passed/charge_time)*(max_damage-damage_range_holder[0]) + damage_range_holder[0]
+		self.hitbox.knockback_strength.y = (time_passed/charge_time)*(max_y_knockback-y_knockback_holder) + y_knockback_holder
+	pass
+	
+func move_reset():
+	self.hitbox.damage_range[0] = self.damage_range_holder[0]
+	self.hitbox.damage_range[1] = self.damage_range_holder[1]
+	self.hitbox.knockback_strength.y = self.y_knockback_holder
+	time_passed = 0
+	pass
+
+func _ready():
+	self.damage_range_holder[0] = self.hitbox.damage_range[0]
+	self.damage_range_holder[1] = self.hitbox.damage_range[1]
+	self.y_knockback_holder = self.hitbox.knockback_strength.y
+
--- src/Scripts/MoveController.gd
+++ src/Scripts/MoveController.gd
@@ -11,38 +11,35 @@ var sprite
 var hurtbox : Node
 var hb_flipped = false
 
+#var move_key = ""
+var move_placeholder = null
 var move_input
+
 
 # ---------------- FUNCTIONS ---------------- #
 
 func attack(move):
-	if !(owner_char.anim_tree_state_machine.get_current_node() in owner_char.MOVE_MAP_NAMES) and !owner_char.blocking:	
-
-		self.move_input = move.move_input
-		
-		play_animation()
-		flip_hurtbox()
-		
-		# do the specific action
-		match move.move_type:
-			"MELEE":
-				pass
-			"GRAB":
-				pass
-			"GRAPPLE":
-				pass
-			"HITSCAN":
-				pass
-			"PROJECTILE":
-				for i in range(move.move_data[0]):
-					var PROJECTILE: PackedScene = load(move.projectile_path)
-					if PROJECTILE:
-						var projectile = PROJECTILE.instantiate()
-						self.owner_char.get_parent().get_parent().add_child(projectile)
-						projectile.global_position = self.owner_char.global_position
-						projectile.owner_char = self.owner_char
-						await get_tree().create_timer(move.move_data[1][i-1]).timeout
-						projectile.emit()
+	
+	flip_hurtbox()
+	match move.move_type:
+		"MELEE":
+			pass
+		"GRAB":
+			pass
+		"GRAPPLE":
+			pass
+		"HITSCAN":
+			pass
+		"PROJECTILE":
+			for i in range(move.move_data[0]):
+				var PROJECTILE: PackedScene = load(move.projectile_path)
+				if PROJECTILE:
+					var projectile = PROJECTILE.instantiate()
+					self.owner_char.get_parent().get_parent().add_child(projectile)
+					projectile.global_position = self.owner_char.global_position
+					projectile.owner_char = self.owner_char
+					await get_tree().create_timer(move.move_data[1][i-1]).timeout
+					projectile.emit()
 
 
 func flip_hurtbox():
@@ -59,13 +56,85 @@ func play_animation():
 
 func anim_finished(anim_name):
 	if anim_name == move_input:
+		if move_placeholder:
+			move_placeholder.move_reset()
+			move_placeholder = null
 		clean()
 		owner_char._update_core_animations()
 		owner_char.can_move = true
 
+		
+
 
 func clean():
 	self.move_input = null
+	self.move_placeholder = null
+	
+func move_start(move):
+	self.move_input = move.move_input
+	play_animation()
+	if move.is_chargable:
+		move_placeholder = move
+		owner_char.anim_tree.set("parameters/" + self.move_input + "/TimeScale/scale", 0)
+	else:
+		attack(move)
+	pass
+	
+func move_end():
+	if move_placeholder and move_placeholder.is_chargable:
+		owner_char.anim_tree.set("parameters/" + self.move_input + "/TimeScale/scale", 1)
+		attack(move_placeholder)
+#	move_placeholder = null
+	pass
+
+
+func action(event):
+	if !(owner_char.anim_tree_state_machine.get_current_node() in owner_char.MOVE_MAP_NAMES) and !owner_char.blocking:
+		if owner_char.is_on_floor() and event.is_pressed():
+			if event.is_action_pressed("normal_close"):
+				move_start(owner_char.ground_nc)
+			if event.is_action_pressed("normal_far"):
+				move_start(owner_char.ground_nf)
+			if event.is_action_pressed("special_close"):
+				move_start(owner_char.ground_sc)
+			if event.is_action_pressed("special_far"):
+				move_start(owner_char.ground_sf)
+		else:
+			if event.is_action_pressed("normal_close"):
+				move_start(owner_char.air_nc)
+			if event.is_action_pressed("normal_far"):
+				move_start(owner_char.air_nf)
+			if event.is_action_pressed("special_close"):
+				move_start(owner_char.air_sc)
+			if event.is_action_pressed("special_far"):
+				move_start(owner_char.air_sf)
+
+	if event.is_released():
+		if owner_char.is_on_floor():
+			if event.is_action_released("normal_close") and self.move_input == "ground_nc":
+				move_end()
+			if event.is_action_released("normal_far") and self.move_input == "ground_nf":
+				move_end()
+			if event.is_action_released("special_close") and self.move_input == "ground_sc":
+				move_end()
+			if event.is_action_released("special_far") and self.move_input == "ground_sf":
+				move_end()
+		else:
+			if event.is_action_released("normal_close") and self.move_input == "air_nc":
+				move_end()
+			if event.is_action_released("normal_far") and self.move_input == "air_nf":
+				move_end()
+			if event.is_action_released("special_close") and self.move_input == "air_sc":
+				move_end()
+			if event.is_action_released("special_far") and self.move_input == "air_sf":
+				move_end()
+	
+
+func _process(delta):
+	if move_placeholder:
+		move_placeholder.move_charge_effect(delta)
+		pass
+	pass
 
 # ---------------- INIT ---------------- #
 
