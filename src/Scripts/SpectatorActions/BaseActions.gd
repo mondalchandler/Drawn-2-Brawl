680242da68c0e1b3c83054073050ab405070962a
otteliena <otteliena@msoe.edu>
2024-02-26 12:29:57 -0600

    player death code

---
+++ src/Scripts/SpectatorActions/BaseActions.gd
@@ -0,0 +1,40 @@
+extends Node
+class_name SpectatorActions
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func parse_action(event, player_data):
+	if event.is_action_pressed("normal_close"):
+		action1(player_data)
+		pass
+	elif event.is_action_pressed("normal_far"):
+		action2(player_data)
+		pass
+	elif event.is_action_pressed("special_close"):
+		action3(player_data)
+		pass
+	elif event.is_action_pressed("special_far"):
+		action4(player_data)
+		pass
+	pass
+	
+func action1(player_data):
+	pass
+	
+func action2(player_data):
+	pass
+	
+func action3(player_data):
+	pass
+	
+func action4(player_data):
+	pass
